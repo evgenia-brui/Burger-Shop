667fe95d4278597ab65e8a122bf801f22ea51bc9
Evgenia Brui <j-lp@mail.ru>
2020-09-04 20:47:58 +0300

    Добавили функцию удаления заказов

---
+++ src/Components/Modal/ModalItem.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ButtonCheckout } from '../Style/ButtonCheckout';
+import { CountItem } from './CountItem';
+import { useCount } from '../Hooks/useCount';
+import { formatCurrency } from '../Functions/secondaryFunction';
+import { totalPriceItems } from '../Functions/secondaryFunction';
+import { Toppings } from './Toppings';
+import { useToppings } from '../Hooks/useTopping';
+import { useChoices } from '../Hooks/useChoices';
+import { Choices } from './Choices';
+
+
+
+const Overlay = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    z-index: 1000;
+`;
+
+const Modal = styled.div`
+    background-color: #fff;
+    width: 600px;
+    height: 600px
+`;
+
+const Banner = styled.div`
+    width: 100%;
+    height: 200px;
+    background-image: url(${({img}) => img});
+    background-size: cover;
+    background-position: center;
+`;
+
+const Content = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: calc(100% - 200px);
+    padding: 30px;
+`;
+
+const HeaderContent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Pacifico', cursive;
+`;
+
+const TotalPriceItem = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+
+export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
+
+    const counter = useCount();
+    const toppings = useToppings(openItem);
+    const choices = useChoices(openItem);
+    const isEdit = openItem.index > -1;
+
+    const closeModal = e => {
+        if (e.target.id === 'overlay') {
+            setOpenItem(null);
+        }
+    }
+
+    const editOrder = () => {
+        const newOrders = [...orders];
+        newOrders[openItem.index] = order;
+        setOrders(newOrders);
+    }
+
+    const order = {
+        ...openItem,
+        count: counter.count,
+        topping: toppings.toppings,
+        choice: choices.choice,
+    };
+
+    const addToOrder = () => {
+        setOrders([...orders, order]);
+        setOpenItem(null);
+    };
+
+    return (
+        <Overlay id="overlay" onClick={closeModal}>
+            <Modal>
+                <Banner img={openItem.img}/>
+                <Content>
+                    <HeaderContent>
+                        <div>{openItem.name}</div>
+                        <div>{formatCurrency(openItem.price)}</div>
+                    </HeaderContent>
+                    <CountItem {...counter}/>
+                    {openItem.toppings && <Toppings {...toppings}/>}
+                    {openItem.choices && <Choices {...choices} openItem={openItem}/>}
+                    <TotalPriceItem>
+                        <span>Цена:</span>
+                        <span>{formatCurrency(totalPriceItems(order))}</span>
+                    </TotalPriceItem>
+                    <ButtonCheckout 
+                    onClick={isEdit ? editOrder :  addToOrder}
+                    disabled={order.choices && !order.choice} 
+                    >Добавить</ButtonCheckout>
+                </Content> 
+            </Modal>
+        </Overlay>
+    )
+};
---
+++ src/Components/Order/OrderListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import trashImage from '../../image/trash.svg';
+import { totalPriceItems } from '../Functions/secondaryFunction';
+import { formatCurrency } from '../Functions/secondaryFunction';
+
+const OrderItemStyled = styled.li`
+    display: flex;
+    margin: 15px 0;
+    flex-wrap: wrap;
+`;
+const ItemName = styled.span`
+    flex-grow: 1;
+`;
+const ItemPrice = styled.span`
+    margin-left: 20px;
+    margin-right: 10px;
+    min-width: 65px;
+    text-align: right;
+`;
+
+const TrashButton = styled.button`
+    width: 24px;
+    height: 24px;
+    border-color: transparent;
+    background-color: transparent;
+    background-image: url(${trashImage});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    cursor: pointer;
+`;
+const Toppings = styled.div`
+    color: #9a9a9a;
+    font-size: 14px;
+    width: 100%;
+`;
+
+export const OrderListItem = ({ order, index, deleteItem, setOpenItem }) => {
+
+    const topping = order.topping.filter(item => item.checked)
+        .map(item => item.name)
+        .join(', ');
+
+    return (
+        <OrderItemStyled onClick={() => setOpenItem({...order, index})}>
+            <ItemName>{order.name} {order.choice}</ItemName>
+            <span>{order.count}</span>
+            <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
+            <TrashButton onClick={() => deleteItem(index)}/>
+            {topping && <Toppings>Допы: {topping}</Toppings>}
+        </OrderItemStyled>
+)};
