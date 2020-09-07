8711d39312eef0aa2ded110fe382779adbb40882
Evgenia Brui <j-lp@mail.ru>
2020-09-07 14:06:30 +0300

    пофиксили карзину заказов

--- src/Components/Modal/ModalItem.js
+++ src/Components/Modal/ModalItem.js
@@ -63,7 +63,7 @@ const TotalPriceItem = styled.div`
 
 export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
 
-    const counter = useCount();
+    const counter = useCount(openItem.count);
     const toppings = useToppings(openItem);
     const choices = useChoices(openItem);
     const isEdit = openItem.index > -1;
@@ -78,6 +78,7 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
         const newOrders = [...orders];
         newOrders[openItem.index] = order;
         setOrders(newOrders);
+        setOpenItem(null);
     }
 
     const order = {
@@ -111,7 +112,7 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
                     <ButtonCheckout 
                     onClick={isEdit ? editOrder :  addToOrder}
                     disabled={order.choices && !order.choice} 
-                    >Добавить</ButtonCheckout>
+                    >{isEdit ? 'Редактировать' : 'Добавить'}</ButtonCheckout>
                 </Content> 
             </Modal>
         </Overlay>
